e31904df306ada4546700a8d2f0bbe3314acd463
shailendra kumar <shailedrakumar25@gmail.com>
2026-07-04 16:06:34 +0530

    create a linked list

---
+++ Create a linked list.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self,value):
+        self.data = value
+        self.next = None
+first = Node(1)
+second = Node(2)
+third = Node(3)
+first.next = second
+second.next = third
+head = first
+print(head.data)
+print(head.next.data)
+print(head.next.next.data)
