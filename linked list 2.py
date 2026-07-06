7bf7e89de5ed901fda3dddc30f2b301061339ba0
shailendra kumar <shailedrakumar25@gmail.com>
2026-07-06 21:02:41 +0530

    linked list II

---
+++ linked list 2.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self,value):
+        self.data = value
+        self.next = None
+def print__ll(head):
+    temp = head
+    while temp != None:
+        print(temp.data, end=" -> ")
+        temp = temp.next
+    print()
+    return
+def take_input():
+    value = int(input("enter the value of node:- "))
+    head = None
+    while (value!=-1):
+        newNode = Node(value)
+        if(head == None):
+            head=newNode
+        else:
+            temp = head
+            while(temp.next!=None):
+                temp = temp.next
+            temp.next = newNode
+        value = int(input("enter the number of nodes:- "))
+    return head
+newhead = take_input()
+print__ll(newhead)
